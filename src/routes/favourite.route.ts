c2415b336504655b6991367963f0d71ccf63aaf4
Petar Kresoja <pkresoja@singidunum.ac.rs>
2026-04-09 13:30:26 +0200

    Add CRUD for Favourite table

--- src/routes/favourite.route.ts
+++ src/routes/favourite.route.ts
@@ -1,8 +1,29 @@
 import { Router } from "express";
 import { FavouriteService } from "../services/favourite.service";
+import { defineRequest } from "../utils";
 
 export const FavouriteRoute = Router()
 
 FavouriteRoute.get('/', async (req: any, res) => {
-    res.json(await FavouriteService.getFavouritesByUserId(req.user.id))
+    await defineRequest(res, async () => {
+        return await FavouriteService.getAll(req.user.id)
+    })
+})
+
+FavouriteRoute.get('/toy/:id', async (req: any, res) => {
+    await defineRequest(res, async () => {
+        return await FavouriteService.getByToyId(req.params.id, req.user.id)
+    })
+})
+
+FavouriteRoute.post('/toy/:id', async (req: any, res) => {
+    await defineRequest(res, async () => {
+        await FavouriteService.createByToyId(req.params.id, req.user.id)
+    })
+})
+
+FavouriteRoute.delete('/:id', async (req: any, res) => {
+    await defineRequest(res, async () => {
+        await FavouriteService.deleteById(req.params.id, req.user.id)
+    })
 })
--- src/services/favourite.service.ts
+++ src/services/favourite.service.ts
@@ -1,3 +1,4 @@
+import { IsNull } from "typeorm";
 import { AppDataSource } from "../db";
 import { Favourite } from "../entities/Favourite";
 import { ToyService } from "./toy.service";
@@ -5,7 +6,7 @@ import { ToyService } from "./toy.service";
 const repo = AppDataSource.getRepository(Favourite)
 
 export class FavouriteService {
-    static async getFavouritesByUserId(id: number) {
+    static async getAll(userId: number) {
         const data = await repo.find({
             select: {
                 favouriteId: true,
@@ -13,7 +14,8 @@ export class FavouriteService {
                 createdAt: true
             },
             where: {
-                userId: id
+                userId: userId,
+                deletedAt: IsNull()
             }
         })
 
@@ -29,5 +31,48 @@ export class FavouriteService {
         }
 
         return data
+    }
+
+    static async getByToyId(toyId: number, userId: number) {
+        return await repo.findOneOrFail({
+            select: {
+                favouriteId: true,
+                toyId: true,
+                createdAt: true
+            },
+            where: {
+                toyId: toyId,
+                userId: userId,
+                deletedAt: IsNull()
+            }
+        })
+    }
+
+    static async createByToyId(toyId: number, userId: number) {
+        const exists = await repo.existsBy({
+            toyId: toyId,
+            userId: userId,
+            deletedAt: IsNull()
+        })
+
+        if (exists)
+            throw new Error('FAVOURITE_TOY_ALREADY_EXISTS')
+
+        await repo.save({
+            toyId: toyId,
+            userId: userId,
+            createdAt: new Date()
+        })
+    }
+
+    static async deleteById(id: number, userId: number) {
+        const existing = await repo.findOneByOrFail({
+            favouriteId: id,
+            userId: userId,
+            deletedAt: IsNull()
+        })
+
+        existing.deletedAt = new Date()
+        await repo.save(existing)
     }
 }
